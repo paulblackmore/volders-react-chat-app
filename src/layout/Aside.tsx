1e31aac85c54552356634aca85187c8f1bf39554
paul <paulmichaelblackmore@gmail.com>
2025-04-01 09:56:43 +0100

    feat: add basic styling to Aside

--- src/layout/Aside.tsx
+++ src/layout/Aside.tsx
@@ -1,6 +1,9 @@
 export const Aside = ({ children }: { children: React.ReactNode }) => (
-  <aside>
-    <h3>Chat History</h3>
+  <aside className='bg-gray-800'>
+    <header className='flex justify-center p-4 bg-gray-700'>
+      <h3>Chat History</h3>
+    </header>
+    {/* TODO: style session history */}
     {children}
   </aside>
 );
