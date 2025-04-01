1c01d560c40a5ab46bbf33b911f19b4ff2299695
paul <paulmichaelblackmore@gmail.com>
2025-04-01 12:08:55 +0100

    fix: make aside title bigger

--- src/layout/Aside.tsx
+++ src/layout/Aside.tsx
@@ -1,7 +1,7 @@
 export const Aside = ({ children }: { children: React.ReactNode }) => (
   <aside className='bg-gray-800'>
     <header className='flex justify-center p-4 bg-gray-700'>
-      <h3>Chat History</h3>
+      <h3 className='text-2xl'>Chat History</h3>
     </header>
     {/* TODO: style session history */}
     {children}
