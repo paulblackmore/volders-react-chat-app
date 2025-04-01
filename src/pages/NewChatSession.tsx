17952edf20be08448703fd6e2d60673b77ba1bc5
paul <paulmichaelblackmore@gmail.com>
2025-04-01 15:26:34 +0100

    feat: add loading and error states to NewChatSession comp

--- src/pages/NewChatSession.tsx
+++ src/pages/NewChatSession.tsx
@@ -1,18 +1,33 @@
 import { StyledButton } from '../components/StyledButton';
 import { useGenerateNewChatSession } from '../hooks';
 import { v4 as uuidv4 } from 'uuid';
+import { useNavigate } from 'react-router-dom';
+import { Loading } from '../components/Loading';
+import { Error } from '../components/Error';
 
 export const NewChatSession = () => {
-  const generateNewChatSession = useGenerateNewChatSession();
+  const navigate = useNavigate();
+  const { mutate, isPending, isError } = useGenerateNewChatSession();
 
-  return (
+  return isPending ? (
+    <Loading />
+  ) : isError ? (
+    <Error />
+  ) : (
     <div className='flex flex-col justify-center items-center gap-4 h-screen'>
       <h3 className='text-2xl'>Start a new chat by clicking below</h3>
       <StyledButton
         onClick={() => {
-          generateNewChatSession.mutate({
-            sessionId: uuidv4(),
-          });
+          mutate(
+            {
+              sessionId: uuidv4(),
+            },
+            {
+              onSuccess: ({ sessionId }: { sessionId: string }) => {
+                navigate(`session/${sessionId}`);
+              },
+            }
+          );
         }}
       >
         Start new chat
