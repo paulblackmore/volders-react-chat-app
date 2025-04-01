8576a101e465108cd8413105d822bb7f5c603911
paul <paulmichaelblackmore@gmail.com>
2025-04-01 17:38:28 +0100

    refactor: move new session logic into features folder

--- src/pages/NewChatSession.tsx
+++ src/pages/NewChatSession.tsx
@@ -1,37 +1,31 @@
-import { StyledButton } from '../components/StyledButton';
 import { useGenerateNewChatSession } from '../hooks';
 import { v4 as uuidv4 } from 'uuid';
 import { useNavigate } from 'react-router-dom';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
+import { StartNewChatSession } from '../features/sessions/StartNewChatSession';
 
 export const NewChatSession = () => {
   const navigate = useNavigate();
   const { mutate, isPending, isError } = useGenerateNewChatSession();
+
+  const handleNewSession = () =>
+    mutate(
+      {
+        sessionId: uuidv4(),
+      },
+      {
+        onSuccess: ({ sessionId }: { sessionId: string }) => {
+          navigate(`session/${sessionId}`);
+        },
+      }
+    );
 
   return isPending ? (
     <Loading />
   ) : isError ? (
     <Error />
   ) : (
-    <div className='flex flex-col justify-center items-center gap-4 h-screen'>
-      <h3 className='text-2xl'>Start a new chat by clicking below</h3>
-      <StyledButton
-        onClick={() => {
-          mutate(
-            {
-              sessionId: uuidv4(),
-            },
-            {
-              onSuccess: ({ sessionId }: { sessionId: string }) => {
-                navigate(`session/${sessionId}`);
-              },
-            }
-          );
-        }}
-      >
-        Start new chat
-      </StyledButton>
-    </div>
+    <StartNewChatSession handleNewSession={handleNewSession} />
   );
 };
