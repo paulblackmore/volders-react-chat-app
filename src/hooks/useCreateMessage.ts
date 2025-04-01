743f65a142a9d7c5c2caa5d22305c607995b6b59
paul <paulmichaelblackmore@gmail.com>
2025-04-01 14:13:57 +0100

    feat: create hook to create a message

---
+++ src/hooks/useCreateMessage.ts
@@ -0,0 +1,12 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { createMesssage } from '../services';
+
+export const useCreateMessage = (sessionId: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: ['messages', sessionId],
+    mutationFn: createMesssage,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['messages'] }),
+  });
+};
