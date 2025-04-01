6cad4011da258869b997219207b699bf40787096
paul <paulmichaelblackmore@gmail.com>
2025-04-01 14:34:59 +0100

    feat: setup router and use Link comp to navigate within chat history

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import { SessionList } from './components/SessionList';
+import { SessionList } from './features/sessions/SessionList';
 import { AppContainer } from './layout/AppContainer';
 import { Aside } from './layout/Aside';
 import { Section } from './layout/Section';
 import { ChatSession } from './pages/ChatSession';
-// import { NewChatSession } from './pages/NewChatSession';
+import { NewChatSession } from './pages/NewChatSession';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AppContainer>
-        <Aside>
-          <SessionList />
-        </Aside>
-        <Section>
-          {/* <NewChatSession /> */}
-          <ChatSession />
-        </Section>
-      </AppContainer>
+      <BrowserRouter>
+        <AppContainer>
+          <Aside>
+            <SessionList />
+          </Aside>
+          <Section>
+            <Routes>
+              <Route index element={<NewChatSession />} />
+              <Route path='session/:sessionId' element={<ChatSession />} />
+            </Routes>
+          </Section>
+        </AppContainer>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
--- src/features/sessions/SessionList.tsx
+++ src/features/sessions/SessionList.tsx
@@ -3,15 +3,19 @@ import { Session } from '../../types';
 import { Loading } from '../../components/Loading';
 import { Error } from '../../components/Error';
 import { EmptyChatHistory } from '../../components/EmptyChatHistory';
+import { Link } from 'react-router-dom';
 
 const SessionItem = ({ sessionId }: { sessionId: string }) => (
   // TODO: hightlight active chat
-  <button
+  <Link
     className='cursor-pointer p-4 border hover:border-green-500 w-full'
     key={sessionId}
+    to={{
+      pathname: `session/${sessionId}`,
+    }}
   >
     {sessionId}
-  </button>
+  </Link>
 );
 
 export const SessionList = () => {
