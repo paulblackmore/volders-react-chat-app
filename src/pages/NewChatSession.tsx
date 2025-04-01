fab0ee7d4498d77b9df0303d6ef1a48d6645eef8
paul <paulmichaelblackmore@gmail.com>
2025-04-01 12:16:52 +0100

    fix: typo in title

--- src/pages/NewChatSession.tsx
+++ src/pages/NewChatSession.tsx
@@ -6,7 +6,7 @@ export const NewChatSession = () => {
 
   return (
     <div className='flex flex-col justify-center items-center gap-4 h-screen'>
-      <h3 className='text-2xl'>Stat a new chat by clicking link below</h3>
+      <h3 className='text-2xl'>Start a new chat by clicking below</h3>
       <button
         className='bg-transparent hover:bg-green-500 white-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded cursor-pointer'
         onClick={() => {
