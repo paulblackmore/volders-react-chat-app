d09e2e2ad5b28bcbb873fd35bbff263013df25ca
paul <paulmichaelblackmore@gmail.com>
2025-04-01 12:57:33 +0100

    feat: create ChatItem comp

--- src/pages/ChatSession.tsx
+++ src/pages/ChatSession.tsx
@@ -1,8 +1,22 @@
 import { StyledButton } from '../components/StyledButton';
 
+const ChatItem = () => (
+  <div className='flex flex-col justify-center items-center bg-slate-500 rounded-lg p-4 w-3/4 gap-4'>
+    <p>Chat Item</p>
+    <footer className='flex justify-between w-full'>
+      <span className='text-sm'>time</span>
+      <span className='text-sm'>Status</span>
+    </footer>
+  </div>
+);
+
 export const ChatSession = () => {
   return (
     <div className='flex flex-col justify-center items-center gap-4 h-screen'>
+      <div className='flex flex-col gap-4 justify-start items-center h-screen w-full mt-4'>
+        {/* TODO: loop over chat items after fetching from api */}
+        <ChatItem />
+      </div>
       <footer className='fixed bottom-0 w-2/4 mb-4'>
         <form>
           <label
