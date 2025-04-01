436290f821376d2c704248aeb839e496fb140c4b
paul <paulmichaelblackmore@gmail.com>
2025-04-01 19:49:53 +0100

    feat: add border colorfor active session

--- src/features/sessions/SessionList.tsx
+++ src/features/sessions/SessionList.tsx
@@ -3,30 +3,30 @@ import { Session } from '../../types';
 import { Loading } from '../../components/Loading';
 import { Error } from '../../components/Error';
 import { EmptyChatHistory } from '../../components/EmptyChatHistory';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const SessionItem = ({ sessionId }: { sessionId: string }) => (
-  // TODO: hightlight active chat
-  <Link
-    className='cursor-pointer p-4 border hover:border-green-500 w-full'
-    key={sessionId}
-    to={{
-      pathname: `session/${sessionId}`,
-    }}
-  >
-    {sessionId}
-  </Link>
-);
+const SessionItem = ({ id }: { id: string }) => {
+  const location = useLocation();
+  const sessionId = location.pathname.split('/')[2];
+  return (
+    <Link
+      className={`cursor-pointer p-4 border hover:border-green-500 w-full ${
+        sessionId === id ? 'border-orange-500' : ''
+      }`}
+      key={id}
+      to={{
+        pathname: `session/${id}`,
+      }}
+    >
+      {id}
+    </Link>
+  );
+};
 
 export const SessionList = () => {
-  const {
-    data: sessions,
-    isLoading,
-    isError,
-    isRefetching,
-  } = useFetchSessions();
+  const { data: sessions, isLoading, isError } = useFetchSessions();
 
-  return isLoading || isRefetching ? (
+  return isLoading ? (
     <Loading />
   ) : isError ? (
     <Error />
@@ -34,7 +34,7 @@ export const SessionList = () => {
     <div className='flex flex-col justify-center items-center gap-3'>
       {sessions?.length ? (
         sessions.map((session: Session) => (
-          <SessionItem key={session.sessionId} sessionId={session.sessionId} />
+          <SessionItem key={session.sessionId} id={session.sessionId} />
         ))
       ) : (
         <EmptyChatHistory />
