500e293e4280bac8df234b3ab230cd57505df7e9
paul <paulmichaelblackmore@gmail.com>
2025-04-01 12:13:45 +0100

    feat: create reusable Loading and Error components

---
+++ src/components/Error.tsx
@@ -0,0 +1,5 @@
+export const Error = () => (
+  <div className='flex justify-center items-center h-screen'>
+    <p className='text-center'>Error while fetching your data</p>
+  </div>
+);
---
+++ src/components/Loading.tsx
@@ -0,0 +1,5 @@
+export const Loading = () => (
+  <div className='flex justify-center items-center h-screen'>
+    <p className='text-center'>Loading...</p>
+  </div>
+);
