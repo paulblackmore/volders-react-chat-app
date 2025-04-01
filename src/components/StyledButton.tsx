f549663378879465b9d8c539d527a0c767d0a1e7
paul <paulmichaelblackmore@gmail.com>
2025-04-01 13:43:05 +0100

    fix: add type with a default of button to StyledButton props

--- src/components/StyledButton.tsx
+++ src/components/StyledButton.tsx
@@ -1,11 +1,13 @@
 type Props = {
   children: React.ReactNode;
   onClick: () => void;
+  type?: 'submit' | 'button';
 };
 
-export const StyledButton = ({ children, onClick }: Props) => (
+export const StyledButton = ({ children, onClick, type = 'button' }: Props) => (
   <button
     className='bg-transparent hover:bg-green-500 white-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded cursor-pointer'
+    type={type}
     onClick={onClick}
   >
     {children}
