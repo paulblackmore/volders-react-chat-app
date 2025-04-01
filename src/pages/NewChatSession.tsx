1bbd3a38cab7af6ad1cfb19371d6c1b953340601
paul <paulmichaelblackmore@gmail.com>
2025-04-01 12:03:21 +0100

    feat: connect generateNewChatSession mutation to button

--- src/pages/NewChatSession.tsx
+++ src/pages/NewChatSession.tsx
@@ -1,8 +1,20 @@
+import { useGenerateNewChatSession } from '../hooks';
+import { v4 as uuidv4 } from 'uuid';
+
 export const NewChatSession = () => {
+  const generateNewChatSession = useGenerateNewChatSession();
+
   return (
     <div className='flex flex-col justify-center items-center gap-4 h-screen'>
       <h3 className='text-2xl'>Stat a new chat by clicking link below</h3>
-      <button className='bg-transparent hover:bg-green-500 white-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded cursor-pointer'>
+      <button
+        className='bg-transparent hover:bg-green-500 white-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded cursor-pointer'
+        onClick={() => {
+          generateNewChatSession.mutate({
+            sessionId: uuidv4(),
+          });
+        }}
+      >
         Start new chat
       </button>
     </div>
