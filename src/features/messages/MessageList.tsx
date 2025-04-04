e3c60c3edaad7ce63538932b33a1809e1a4ec435
paul <paulmichaelblackmore@gmail.com>
2025-04-04 19:54:19 +0100

    feat: improve message layout

--- src/features/messages/MessageList.tsx
+++ src/features/messages/MessageList.tsx
@@ -5,22 +5,25 @@ import { MessageFromApi } from '../../types/message';
 import { messageStatusIndicator } from '../../helpers';
 
 const MessageItem = ({ message }: { message: MessageFromApi }) => {
+  const messagePosition = message.kind ? 'items-end' : 'items-start';
   const messageBgColor = message.status
     ? messageStatusIndicator[message.status]
     : 'bg-green-600';
 
   return (
-    <div
-      className={`flex flex-col justify-center items-center ${messageBgColor} rounded-lg p-2 w-3/4 gap-1`}
-    >
-      <p className='text-sm font-semibold p-1'>{message.text}</p>
-      <footer className='flex justify-between w-full'>
-        <span className='text-xs'>{message.timestamp}</span>
-        <span className='text-xs text-transform: capitalize'>
-          {/* TODO: ask for status to be added to message entity */}
-          {message.status ? message.status : 'sent'}
-        </span>
-      </footer>
+    <div className={`flex flex-col gap-4 ${messagePosition} w-210`}>
+      <div
+        className={`flex flex-col justify-center items-center ${messageBgColor} rounded-lg p-2 w-1/2 gap-1`}
+      >
+        <p className='font-semibold p-1'>{message.text}</p>
+        <footer className='flex justify-between w-full'>
+          <span className='text-xs'>{message.timestamp}</span>
+          <span className='text-xs text-transform: capitalize'>
+            {/* TODO: ask for status to be added to message entity */}
+            {message.status ? message.status : 'sent'}
+          </span>
+        </footer>
+      </div>
     </div>
   );
 };
@@ -28,11 +31,10 @@ const MessageItem = ({ message }: { message: MessageFromApi }) => {
 export const MessageList = () => {
   const { sessionId } = useParams();
   const { data: messages } = useFetchMessagesBySessionId(sessionId || '');
-
   return (
     <div className='h-screen w-full mt-4'>
       {messages?.length ? (
-        <div className=' flex flex-col gap-2 justify-start items-center w-full h-6/7 overflow-y-scroll'>
+        <div className='flex flex-col justify-start items-center w-full h-6/7 overflow-y-scroll'>
           {messages.map((message: MessageFromApi) => (
             <MessageItem key={message.id} message={message} />
           ))}
