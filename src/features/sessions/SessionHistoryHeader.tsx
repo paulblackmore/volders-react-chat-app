0b4cec2409308e31125502b38fe24e1868243ff6
paul <paulmichaelblackmore@gmail.com>
2025-04-07 19:54:11 +0200

    refactor: remove span from Link

--- src/features/sessions/SessionHistoryHeader.tsx
+++ src/features/sessions/SessionHistoryHeader.tsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 export const SessionHistoryHeader = () => (
   <header className='flex justify-between items-center p-4'>
     <h3 className='text-2xl'>Session History</h3>
-    <Link to='/'>
-      <span className='text-sm'>Home</span>
+    <Link to='/' className='text-sm'>
+      Home
     </Link>
   </header>
 );
