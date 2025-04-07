3e54912bd5ddbc9fa1349f75d78dd19f9554965a
paul <paulmichaelblackmore@gmail.com>
2025-04-07 18:27:10 +0200

    refactor: export context and provider from context file

--- src/features/messages/contexts/FailedMessageContext.tsx
+++ src/features/messages/contexts/FailedMessageContext.tsx
@@ -1,12 +1,30 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 import { MessageInput } from '../../../types/message';
 
-type Props = {
+type ContextProps = {
   failedMessages: MessageInput[];
   setFailedMessages: React.Dispatch<React.SetStateAction<MessageInput[]>>;
 };
 
-export const FailedMessageContext = createContext<Props>({
+type ProviderProps = {
+  children: React.ReactNode;
+};
+
+const FailedMessageContext = createContext<ContextProps>({
   failedMessages: [],
   setFailedMessages: () => {},
 });
+
+const FailedMessageProvider = ({ children }: ProviderProps) => {
+  const [failedMessages, setFailedMessages] = useState<MessageInput[]>([]);
+
+  return (
+    <FailedMessageContext.Provider
+      value={{ failedMessages, setFailedMessages }}
+    >
+      {children}
+    </FailedMessageContext.Provider>
+  );
+};
+
+export { FailedMessageContext, FailedMessageProvider };
